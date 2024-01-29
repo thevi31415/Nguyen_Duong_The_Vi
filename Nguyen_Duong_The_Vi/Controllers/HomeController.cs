444cdf36088a68690b70340288f4ffdbf4b84523
thevi31415 <92256900+thevi31415@users.noreply.github.com>
2024-01-30 06:47:42 +0700

    Add project files.

---
+++ Nguyen_Duong_The_Vi/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nguyen_Duong_The_Vi.Models;
+using System.Diagnostics;
+
+namespace Nguyen_Duong_The_Vi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Nguyen_Duong_The_Vi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nguyen_Duong_The_Vi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
