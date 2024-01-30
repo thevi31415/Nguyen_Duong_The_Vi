2d20b5065d32ef6301527a364089ad9f0d03f28b
thevi31415 <92256900+thevi31415@users.noreply.github.com>
2024-01-30 11:29:23 +0700

    Chỉnh sửa trang chủ, trang About

--- Nguyen_Duong_The_Vi/Controllers/HomeController.cs
+++ Nguyen_Duong_The_Vi/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Nguyen_Duong_The_Vi.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
