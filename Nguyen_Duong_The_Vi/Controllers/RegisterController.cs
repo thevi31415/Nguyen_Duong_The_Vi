b275d73166480c2b5198d4df48b40af2dbf25fc3
thevi31415 <92256900+thevi31415@users.noreply.github.com>
2024-02-15 16:20:14 +0700

    Giao diện trang đăng ký

---
+++ Nguyen_Duong_The_Vi/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nguyen_Duong_The_Vi.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
