9c422d3ee622803f879c690c9deae9b53363a68b
thevi31415 <92256900+thevi31415@users.noreply.github.com>
2024-02-18 18:12:17 +0700

    Thêm trang forum

---
+++ Nguyen_Duong_The_Vi/Controllers/ForumController.cs
@@ -0,0 +1,33 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Nguyen_Duong_The_Vi.Data;
+using Nguyen_Duong_The_Vi.Models;
+
+namespace Nguyen_Duong_The_Vi.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+    
+        public ForumController(ApplicationDbContext db)
+        {
+            _db = db;
+        
+        }
+        public IActionResult Index()
+        {
+            ThongTin firstThongTin = _db.thongTins.FirstOrDefault();
+            if (firstThongTin == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                ViewBag.ThongTin = firstThongTin;
+
+            }
+            return View();
+        }
+    }
+}
